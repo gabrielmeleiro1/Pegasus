13bd9fa9474745b8d426f9478e6186672b7df14a
Gabriel <gabriels.meleiro@gmail.com>
2025-02-17 21:19:09 +0100

    Adding

---
+++ limit.cpp
@@ -0,0 +1,50 @@
+#include "limit.h"
+#include <iterator>   // for std::find (if needed)
+#include <cassert>    // for assert
+
+Limit::Limit(double price)
+    : m_price(price),
+      m_totalVolume(0.0)
+{
+}
+
+void Limit::addOrder(Order* order)
+{
+    // Insert at the back of the FIFO list
+    m_orders.push_back(order);
+
+    // Increase total volume by the order's available quantity
+    double remainingQty = order->getQuantity() - order->getFilledQty();
+    updateTotalVolume(remainingQty);
+}
+
+void Limit::removeOrder(Order* order)
+{
+    // We find the order in O(n) time in std::list.
+    // For a learning project, this is okay; in production HFT, you'd optimize this.
+    auto it = std::find(m_orders.begin(), m_orders.end(), order);
+    if (it != m_orders.end()) {
+        // Decrease total volume
+        double remainingQty = order->getQuantity() - order->getFilledQty();
+        updateTotalVolume(-remainingQty);
+
+        // Erase it from the list
+        m_orders.erase(it);
+    }
+}
+
+Order* Limit::frontOrder() const
+{
+    if (m_orders.empty()) {
+        return nullptr;
+    }
+    return m_orders.front();
+}
+
+Order* Limit::backOrder() const
+{
+    if (m_orders.empty()) {
+        return nullptr;
+    }
+    return m_orders.back();
+}
